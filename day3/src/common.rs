114027cc3a1e4be6b114d46bf446b567dc78489c
Gyanbu <gyanbu@protonmail.com>
2025-12-03 18:49:13 +0000

    Implement input parsing and solution logic for day 3

--- day3/src/common.rs
+++ day3/src/common.rs
@@ -1,5 +1,8 @@
-pub type Input = ();
+pub type Input = Vec<Vec<u8>>;
 
 pub fn parse(input: String) -> Input {
-    todo!()
+    input
+        .lines()
+        .map(|bank| bank.chars().map(|battery| battery as u8 - b'0').collect())
+        .collect()
 }
--- day3/src/part1.rs
+++ day3/src/part1.rs
@@ -1,5 +1,24 @@
 use crate::common::Input;
 
 pub fn solve(input: &Input) -> String {
-    todo!()
+    let mut answer: u128 = 0;
+    for bank in input {
+        let mut left_battery = (0, 0);
+        for (i, battery) in bank[..bank.len() - 1].iter().enumerate() {
+            if *battery > left_battery.1 {
+                left_battery = (i, *battery);
+            }
+        }
+
+        let mut right_battery = 0;
+        for battery in &bank[left_battery.0 + 1..] {
+            if *battery > right_battery {
+                right_battery = *battery;
+            }
+        }
+
+        let joltage = left_battery.1 * 10 + right_battery;
+        answer += joltage as u128;
+    }
+    answer.to_string()
 }
