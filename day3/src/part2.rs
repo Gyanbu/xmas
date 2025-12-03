08af8e8877335a52e40aa0cad1aa7c6706ee2440
Gyanbu <gyanbu@protonmail.com>
2025-12-03 22:38:36 +0000

    Implement solution logic for part 2, calculating total jolts from battery banks

--- day3/src/part2.rs
+++ day3/src/part2.rs
@@ -1,5 +1,29 @@
+use std::array;
+
 use crate::common::Input;
 
 pub fn solve(input: &Input) -> String {
-    todo!()
+    let mut answer: u128 = 0;
+    for bank in input {
+        let bank_length = bank.len();
+        let mut chosen_batteries: [usize; 12] = array::from_fn(|i| bank_length - 12 + i);
+        let mut previous_battery: isize = -1;
+        for battery in chosen_batteries.iter_mut() {
+            let mut highest_left_battery: (usize, u8) = (*battery, 0);
+            for i in (previous_battery + 1) as usize..=*battery {
+                if bank[i] > highest_left_battery.1 {
+                    highest_left_battery = (i, bank[i]);
+                }
+            }
+            *battery = highest_left_battery.0;
+            previous_battery = highest_left_battery.0 as isize;
+        }
+
+        let mut jolts: u128 = 0;
+        for (i, battery) in chosen_batteries.into_iter().rev().enumerate() {
+            jolts += bank[battery] as u128 * 10u128.pow(i as u32);
+        }
+        answer += jolts;
+    }
+    answer.to_string()
 }
