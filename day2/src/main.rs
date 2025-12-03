4e44a2baf4268b1bcda46e79910ec85bb1285ff4
Gyanbu <gyanbu@protonmail.com>
2025-12-03 18:20:39 +0000

    Remove commented-out input string from main function

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -4,8 +4,6 @@ mod part2;
 
 fn main() {
     let input = utils::read_input();
-    // let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
-    // .to_string();
     let parsed_data = common::parse(input);
 
     let result1 = part1::solve(&parsed_data);
