ac23d6ca67292902449586b3249eaaa26b9e5525
Gyanbu <gyanbu@protonmail.com>
2025-12-05 19:53:13 +0000

    Implement first solution idea for part 2, doesn't work

--- day5/src/part2.rs
+++ day5/src/part2.rs
@@ -1,5 +1,24 @@
 use crate::common::Input;
 
 pub fn solve(input: &Input) -> String {
-    todo!()
+    let mut ranges = input.0.clone();
+    ranges.sort_by(|a, b| a.start().cmp(b.start()));
+
+    let mut answer: u128 = 0;
+    let mut furthest_index = 0;
+    for range in ranges {
+        if *range.end() <= furthest_index {
+            continue;
+        }
+        if *range.start() <= furthest_index {
+            let offset = range.end() - furthest_index;
+            furthest_index = *range.end();
+            answer += (range.count() - offset) as u128;
+        } else {
+            furthest_index = *range.end();
+            answer += range.count() as u128;
+        }
+    }
+
+    answer.to_string()
 }
