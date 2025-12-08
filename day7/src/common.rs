4fbacc025d43bea29f2f4224a27a728802ac131f
Gyanbu <gyanbu@protonmail.com>
2025-12-08 22:02:57 +0000

    Implement input parsing and solution to part 1

--- day7/src/common.rs
+++ day7/src/common.rs
@@ -1,5 +1,67 @@
-pub type Input = ();
+use std::collections::HashSet;
+
+pub type Input = Manifold;
 
 pub fn parse(input: String) -> Input {
-    todo!()
+    let mut start = None;
+    let mut splitters = Vec::new();
+    for (y, row) in input.lines().enumerate() {
+        for (x, c) in row.chars().enumerate() {
+            match c {
+                'S' => start = Some(Point::new(x, y)),
+                '^' => splitters.push(Point::new(x, y)),
+                '.' => continue,
+                _ => panic!(),
+            }
+        }
+    }
+    Manifold::new(
+        start.unwrap(),
+        splitters,
+        input.lines().next().unwrap().len(),
+        input.lines().count(),
+    )
+}
+
+#[derive(PartialEq, Eq, Hash, Debug, Clone, Copy)]
+pub struct Point {
+    pub x: usize,
+    pub y: usize,
+}
+
+impl Point {
+    pub fn new(x: usize, y: usize) -> Self {
+        Self { x, y }
+    }
+}
+
+#[derive(PartialEq, Eq, Hash, Debug, Clone, Copy)]
+pub struct Tachyon {
+    pub pos: Point,
+}
+
+impl Tachyon {
+    pub fn new(pos: Point) -> Self {
+        Self { pos }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Manifold {
+    pub tachyons: HashSet<Tachyon>,
+    pub width: usize,
+    pub height: usize,
+    pub splitters: Vec<Point>,
+}
+
+impl Manifold {
+    pub fn new(start: Point, splitters: Vec<Point>, width: usize, height: usize) -> Self {
+        let tachyon = Tachyon::new(start);
+        Self {
+            tachyons: HashSet::from([tachyon]),
+            splitters,
+            width,
+            height,
+        }
+    }
 }
--- day7/src/part1.rs
+++ day7/src/part1.rs
@@ -1,5 +1,53 @@
-use crate::common::Input;
+use std::{collections::HashSet, mem};
+
+use crate::common::{Input, Manifold, Point, Tachyon};
 
 pub fn solve(input: &Input) -> String {
-    todo!()
+    let mut manifold = input.clone();
+    manifold.simulate().to_string()
+}
+
+impl Tachyon {
+    fn split(&self) -> Vec<Tachyon> {
+        let mut buf = Vec::new();
+        for x_offset in [-1, 1] {
+            let Some(new_x) = self.pos.x.checked_add_signed(x_offset) else {
+                continue;
+            };
+            buf.push(Tachyon::new(Point::new(new_x, self.pos.y)));
+        }
+        buf
+    }
+
+    fn step(&mut self) {
+        self.pos.y += 1;
+    }
+}
+
+impl Manifold {
+    fn step(&mut self) -> usize {
+        let mut hit_splitters = HashSet::new();
+        for mut tachyon in mem::take(&mut self.tachyons) {
+            tachyon.step();
+            if self.splitters.contains(&tachyon.pos) {
+                hit_splitters.insert(tachyon.pos);
+                for tachyon in tachyon.split() {
+                    if tachyon.pos.x > 0 && tachyon.pos.x < self.width {
+                        self.tachyons.insert(tachyon);
+                    }
+                }
+            } else {
+                self.tachyons.insert(tachyon);
+            }
+        }
+        hit_splitters.len()
+    }
+
+    fn simulate(&mut self) -> usize {
+        let mut hit_splitters = 0;
+        for _ in 0..self.height - 1 {
+            hit_splitters += self.step();
+        }
+        hit_splitters
+    }
 }
